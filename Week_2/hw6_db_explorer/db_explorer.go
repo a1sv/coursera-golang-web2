cfc3a0b5c37ca16b6db41a32ee7a7a0f7a43d469
Alex <al.sh@outlook.com>
2020-06-24 02:39:23 +0500

    some basic dbexplorer methods

--- Week_2/hw6_db_explorer/db_explorer.go
+++ Week_2/hw6_db_explorer/db_explorer.go
@@ -5,59 +5,135 @@ package main
 
 import (
 	"database/sql"
-	"encoding/json"
-	"log"
+	"fmt"
 	"net/http"
-	"sync"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
-type Handler struct {
+// DbScanner сканирует структуру базы
+type DbScanner struct {
 	DB *sql.DB
 }
 
+// DbExplorer основной метод, который оперирует с базой данных
+type DbExplorer struct {
+	DB     *sql.DB
+	tables []string
+}
+
+// Item пока не используем
 type Item struct {
-	Id          int
+	ID          int
 	Title       string
 	Description string
 	Updated     sql.NullString
 }
 
+// Table описывает таблицу в базе
 type Table struct {
 	Name string
-	mu   sync.Mutex
+	key  int
 }
 
-//NewDbExplorer lists rows
-func NewDbExplorer(db *sql.DB) (*http.ServeMux, error) {
-	h := &Handler{DB: db}
-	handler := h.ListTables
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", handler)
-	return mux, nil
-}
-
-func (h *Handler) ListTables(w http.ResponseWriter, r *http.Request) {
-	items := []*Table{}
-
-	rows, err := h.DB.Query("SHOW TABLES")
+// ListTableNames перечисляет названия таблиц в базе
+func (s *DbScanner) ListTableNames() (tableNames []string, err error) {
+	rows, err := s.DB.Query("SHOW TABLES")
 	if err != nil {
-		log.Fatal(err)
+		return nil, fmt.Errorf("failed to fetch table names: %s", err)
 	}
-
+	var n string
 	for rows.Next() {
-		it := &Table{}
-		err := rows.Scan(&it.Name)
+		err := rows.Scan(&n)
 		if err != nil {
 			panic(err)
 		}
-		items = append(items, it)
+		tableNames = append(tableNames, n)
 	}
 
 	rows.Close()
-	res, _ := json.Marshal(items)
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(res)
-
+	return tableNames, nil
 }
+
+// NewDbScanner создаёт новый экземпляр сканера базы
+func NewDbScanner(db *sql.DB) *DbScanner {
+	return &DbScanner{db}
+}
+
+// NewDbExplorer создёт новый экземпляр оператора базы данных
+func NewDbExplorer(db *sql.DB) (*DbExplorer, error) {
+	tables, err := NewDbScanner(db).ListTableNames()
+	if err != nil {
+		return nil, fmt.Errorf("failed to make new DbExplorer: %s", err)
+	}
+	return &DbExplorer{db, tables}, nil
+}
+
+// ServeHTTP основной обработчик http запросов
+func (e *DbExplorer) ServeHTTP(w http.ResponseWriter, r *http.Request) {}
+
+//NewDbExplorer lists rows
+// func NewDbExplorer(db *sql.DB) (*http.ServeMux, error) {
+// 	s := &DbScanner{DB: db}
+// 	handler := s.ListTables
+// 	mux := http.NewServeMux()
+// 	mux.HandleFunc("/", handler)
+// 	return mux, nil
+// }
+
+// func (d *DbExplorer) ListTables(w http.ResponseWriter, r *http.Request) {
+// 	tableNames := []string{}
+
+// 	switch r.URL.Path {
+// 	case "/":
+// 		break
+
+// 	default:
+// 		w.Header().Set("Content-Type", "application/json")
+// 		w.Header().Set("Server", "Go!")
+// 		w.WriteHeader(http.StatusNotFound)
+// 		res := struct {
+// 			Error string `json:"error"`
+// 		}{}
+// 		res.Error = "unknown table"
+// 		jsonres, err := json.Marshal(res)
+// 		if err != nil {
+// 			panic(err)
+// 		}
+// 		w.Write(jsonres)
+// 		return
+// 	}
+
+// 	rows, err := h.DB.Query("SHOW TABLES")
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	for rows.Next() {
+// 		it := &Table{}
+// 		err := rows.Scan(&it.Name)
+// 		if err != nil {
+// 			panic(err)
+// 		}
+// 		tableNames = append(tableNames, it.Name)
+// 	}
+
+// 	rows.Close()
+
+// 	res := struct {
+// 		Payload struct {
+// 			Tables []string `json:"tables"`
+// 		} `json:"response"`
+// 	}{}
+// 	res.Payload.Tables = tableNames
+
+// 	jsonres, err := json.Marshal(res)
+
+// 	if err != nil {
+// 		http.Error(w, err.Error(), http.StatusInternalServerError)
+// 		return
+// 	}
+
+// 	w.Header().Set("Content-Type", "application/json")
+// 	w.Write(jsonres)
+// }
